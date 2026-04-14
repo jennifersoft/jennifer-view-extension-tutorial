ec50703752366b580c5e7e76cbe0cd7875ba52c1
alvin <alvin@jennifersoft.com>
2026-04-14 10:42:37 +0900

    시스템 이벤트 kt 버전, 아이디 잘못된 버그 수정

--- src/main/kotlin/com/aries/tutorial2/adapter/SystemEventAdapter.kt
+++ src/main/kotlin/com/aries/tutorial2/adapter/SystemEventAdapter.kt
@@ -8,7 +8,7 @@ import com.aries.extension.util.PropertyUtil
 class SystemEventAdapter : SystemEventHandler {
     override fun on(events: Array<SystemEventData>) {
         LogUtil.info("[SystemEventAdapter] - " +
-                PropertyUtil.getValue("event_adapter", "subject", "Unknown subject"))
+                PropertyUtil.getValue("system_event_adapter", "subject", "Unknown subject"))
 
         for (data in events) {
             LogUtil.info("Subject : " + data.subject)
